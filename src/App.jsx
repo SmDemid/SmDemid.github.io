6189de9267bd84be92542f7fccc6f69dfa1c0c60
SmDemid <114074242+SmDemid@users.noreply.github.com>
2024-12-18 02:36:07 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TodoPage from './pages/TodoPage';
+import DndPage from './pages/Dnd';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/todo" element={<TodoPage />} />
+        <Route path="/dnd" element={<DndPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
