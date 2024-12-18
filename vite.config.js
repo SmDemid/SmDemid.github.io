e9b927f68ea5eefa5e1d7c1517e3c8c23aeedf4e
SmDemid <114074242+SmDemid@users.noreply.github.com>
2024-12-18 10:19:16 +0300

    8 commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: 'https://SmDemid.github.io/SmDemid.github.io/',
+  base: 'https://SmDemid.github.io/SmDemid.github.io/',
 })
