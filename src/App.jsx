f9e8398f05e6282e6c7a8c9106d37ebbd0b6fd0a
SmDemid <114074242+SmDemid@users.noreply.github.com>
2024-12-18 04:27:16 +0300

    3commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TodoPage from './pages/TodoPage';
 import DndPage from './pages/Dnd';
+
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/SmDemid.github.io/" element={<TodoPage />} />
+        <Route path="/" element={<TodoPage />} />
         <Route path="/dnd" element={<DndPage />} />
       </Routes>
     </Router>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://smdemid.github.io/SmDemid.github.io',
+  base: 'https://SmDemid.github.io/SmDemid.github.io',
 })
